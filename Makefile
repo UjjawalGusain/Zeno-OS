5d45d04d529b84c6e1cf2be0cce534c787d1a060
Ujjawal Gusain <ujjawalgusain31@gmail.com>
2026-03-30 17:25:54 +0530

    Complete Makefile

--- Makefile
+++ Makefile
@@ -1,12 +1,34 @@
-ASPARAMS = -f elf32
-ELFPARAMS = -melf_i386
-LINKER_SCRIPT = linker.ld
+OBJECTS = loader.o kmain.o
+CC = gcc
+CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
+LDFLAGS = -T linker.ld -melf_i386
+AS = nasm
+ASFLAGS = -f elf
+
+all: kernel.elf
+
+kernel.elf: $(OBJECTS)
+	ld $(LDFLAGS) $(OBJECTS) -o kernel.elf
+
+os.iso: kernel.elf
+	cp kernel.elf iso/boot/kernel.elf
+	genisoimage -R -b boot/grub/stage2_eltorito \
+	-no-emul-boot -boot-load-size 4 \
+	-A os \
+	-input-charset utf8 \
+    -quiet \
+	-boot-info-table \
+	-o os.iso \
+	iso
+
+run: os.iso
+	bochs -f bochsrc.txt -q
+
+%.o: %.c
+	$(CC) $(CFLAGS) $< -o $@
 
 %.o: %.s
-	nasm $(ASPARAMS) $< -o $@ 
-
-kernel.elf: loader.o
-	ld -T $(LINKER_SCRIPT) $(ELFPARAMS) $^ -o  $@
+	$(AS) $(ASFLAGS) $< -o $@
 
 clean:
-	rm kernel.elf loader.o
+	rm -rf *.o kernel.elf os.iso
--- loader.s
+++ loader.s
@@ -2,17 +2,26 @@ MAGIC_NUMBER equ 0x1BADB002           ; need this in multiboot section to let GR
 FLAGS equ 0x0                         ; multiboot flags
 CHECKSUM equ -(MAGIC_NUMBER + FLAGS)  ; calculating checksum -> checksum + MAGIC + FLAGS = 0
 
-section .multiboot:
+KERNEL_STACK_SIZE equ 4096            ; stack of 4kb
+
+section .multiboot
 align 4                               ; we need to align all labels to addresses divisible by 4
     dd MAGIC_NUMBER                   ; we want GRUB to know that this is multiboot loader, and for that it needs MAGIC NUMBER
     dd FLAGS
     dd CHECKSUM
 
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
+
 
 section .text
+
 global loader                         ; entry symbol for ELF. Makes loader available outside the file
 loader:
     mov eax, 0xCAFEBABE               ; AX register is loaded with this
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; pointing stack pointer to the end of memorhy
 
 .loop:
     jmp .loop                         ; loop forever
